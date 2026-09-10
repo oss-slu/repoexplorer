b710bf0b05be808907f738cb3dc0a4574e81cef1
Grace Gondela <grace.gondela@slu.edu>
2026-09-10 10:18:27 -0500

    Update security route filtering

--- api/src/routes/security.ts
+++ api/src/routes/security.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { BASE_SECURITY } from '../consts';
+import { BASE_SECURITY, FILTERABLE_SECR_FIELDS } from '../consts';
 import type { RespSecurity } from '../types/routes';
 import type { secrData } from '../types/appData';
 import sampleData from '../../data/sample/sampleSecrData.json';
 import { makeAvgScorePerMetricArray } from '../utils/math';
+import { filterData } from '../utils/filter';
 
 const data = sampleData as secrData[];
 const router = Router();
@@ -13,19 +14,26 @@ const SUB_ENDPOINTS = {
     avgScorePerMetric: (data: secrData[]) => makeAvgScorePerMetricArray(data),
 } satisfies Partial<Record<keyof RespSecurity, (data: secrData[]) => RespSecurity[keyof RespSecurity]>>;
 
-router.get(BASE_SECURITY, (_req, res) => {
-    const response: RespSecurity = {
-        securityScorecardByRepo: SUB_ENDPOINTS.securityScorecardByRepo(data),
-        avgScorePerMetric: SUB_ENDPOINTS.avgScorePerMetric(data),
-    };
+router.get(BASE_SECURITY, (req, res) => {
+    const filtered = filterData(data, req.query, FILTERABLE_SECR_FIELDS);
 
-    res.json(response);
+    res.json(
+        Object.fromEntries(
+            Object.entries(SUB_ENDPOINTS).map(([endpoint, fn]) => [
+                endpoint,
+                fn(filtered) as RespSecurity[keyof RespSecurity],
+            ]),
+        ),
+    );
 });
 
 for (const [endpoint, fn] of Object.entries(SUB_ENDPOINTS)) {
-    router.get(`${BASE_SECURITY}/${endpoint}`, (_req, res) => {
-        const value = fn(data) as RespSecurity[keyof RespSecurity];
-        res.json({ [endpoint]: value } satisfies Partial<RespSecurity>);
+    router.get(`${BASE_SECURITY}/${endpoint}`, (req, res) => {
+        const filtered = filterData(data, req.query, FILTERABLE_SECR_FIELDS);
+
+        res.json({
+            [endpoint]: fn(filtered) as RespSecurity[keyof RespSecurity],
+        });
     });
 }
 
