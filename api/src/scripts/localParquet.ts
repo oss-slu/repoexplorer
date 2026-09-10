399c36ae48e1e4f625456d5aea7ca65658b646f5
jdetok <jdeko17@gmail.com>
2026-09-09 20:12:33 -0500

    localParquet.ts now fetches each parquet (REPO, ORGS, SECR)

--- api/src/scripts/localParquet.ts
+++ api/src/scripts/localParquet.ts
@@ -1,17 +1,33 @@
 // fetch the parquet file from the public UC OSPO S3 bucket
-
+import { parseArgs } from 'util';
 import { confirm } from '../utils/cli';
 import { findRecentParquetInDir, getParquet } from '../utils/parquet';
+import type { parqFileOpts } from '../types/scripts';
 
-const existingParq = await findRecentParquetInDir();
-if (existingParq) {
-    const proceed = await confirm(
-        `A parquet file already exists locally (${existingParq}) - continue to fetch new file?`,
-    );
-    if (!proceed) {
-        console.log('Aborted');
-        process.exit(1);
+const { positionals } = parseArgs({ allowPositionals: true });
+const modeOpts = ['REPO', 'SECR', 'ORGS', 'EACH'];
+const mode = positionals[0]?.toUpperCase();
+
+// const runMode: parqFileOpts = (mode === 'REPO' || mode === 'SECR' || mode === 'ORGS' || mode == 'EACH') ? mode : 'REPO';
+const runMode: string = modeOpts.includes(mode) ? mode : 'EACH';
+
+// const QUEUE = ['REPO', 'SECR', 'ORGS'];
+const QUEUE = runMode === 'EACH' ? ['REPO', 'SECR', 'ORGS'] : [runMode];
+
+QUEUE.forEach(async (run) => {
+    console.log(`Beginning ${run} parquet fetch...`);
+    const existingParq = await findRecentParquetInDir(run as parqFileOpts);
+    if (existingParq) {
+        const proceed = await confirm(
+            `A ${run} parquet file already exists locally (${existingParq}) - continue to fetch new file?`,
+        );
+        if (!proceed) {
+            console.log('Aborted');
+            process.exit(1);
+        }
     }
-}
-console.log('Fetching parquet file from UC OSPO S3 bucket...');
-await getParquet();
+
+    await getParquet(run as parqFileOpts);
+});
+
+
---
+++ api/src/types/scripts.ts
@@ -0,0 +1,1 @@
+export type parqFileOpts = 'REPO' | 'SECR' | 'ORGS' | 'EACH';
